89c4a30dccf8ffd7f330a081ffcd1c5d3b8bf04f
Chris Du <justakiss918@gmail.com>
2025-10-15 09:58:25 +0800

    change speedinsights from next to react

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { Bubbles } from "./components/Bubbles";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   return (
