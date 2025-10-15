4aa46163ef7b38577eec41d964156a68f52f1a1e
Chris Du <justakiss918@gmail.com>
2025-10-15 09:47:09 +0800

    Add Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { Bubbles } from "./components/Bubbles";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App() {
   return (
     <div className="min-h-screen">
+      <SpeedInsights />
       <Header />
       <main>
         <Bubbles />
