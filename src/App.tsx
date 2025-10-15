dca8d02fda5cd179f550137e833b9b92d3ad3eae
Chris Du <justakiss918@gmail.com>
2025-10-15 10:25:12 +0800

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { Bubbles } from "./components/Bubbles";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <div className="min-h-screen">
       <SpeedInsights />
+      <Analytics />
       <Header />
       <main>
         <Bubbles />
